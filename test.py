83def8abfcdac6320d0008f0bccac15785d651be
danial sadri <danielsadri01@gmail.com>
2024-04-26 14:51:02 +0330

    update

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from main import *
+from main import add, add_bind, division_bind
+from celery import chain, group, chord, chunks
 
+# -----------------------------------------------------------------------
 # Signatures
 # result = division_bind.signature((5, 5)).delay()
 # print(result)
@@ -15,3 +17,16 @@ from main import *
 # Immutability
 # result = division_bind.apply_async((5, 5), link=add.signature((5, 5), immutable=True))
 # print(result)
+# -----------------------------------------------------------------------
+# result = chain(add.s(5, 5), add_bind.s(5))
+# print(result().get())
+# -----------------------------------------------------------------------
+# result = group(add.s(5, 5), add_bind.s(5, 5))
+# print(result().get())
+# -----------------------------------------------------------------------
+# result = chord(add.s(5, 5), add_bind.s(5, 5))(division_bind.s(5, 5))
+# print(result.get())
+# -----------------------------------------------------------------------
+# result = add.chunks(zip(range(10), range(100)), 10)
+# print(result())
+# -----------------------------------------------------------------------
