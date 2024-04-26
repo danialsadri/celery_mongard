5cfd7d8e129962f7f03378c99dc9cd6ed6ed62d2
danial sadri <danielsadri01@gmail.com>
2024-04-26 12:20:59 +0330

    update

---
+++ test.py
@@ -0,0 +1,17 @@
+from main import *
+
+# Signatures
+# result = division_bind.signature((5, 5)).delay()
+# print(result)
+
+# Partials
+# result = division_bind.signature((5,)).delay(5)
+# print(result)
+
+# Callbacks
+# result = division_bind.apply_async((5, 5), link=add.signature((5,)))
+# print(result)
+
+# Immutability
+# result = division_bind.apply_async((5, 5), link=add.signature((5, 5), immutable=True))
+# print(result)
